aaefbca29f3b949505492f633ee3894250fbb6cf
0957146 <0957146@SW21>
2020-10-27 14:08:31 +1300

    Added scheduler page

---
+++ TestScheduler/TestSchedulerTests/Controllers/BaseControllerTests.cs
@@ -0,0 +1,17 @@
+﻿using TestScheduler.Controllers;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace TestScheduler.Controllers.Tests
+{
+    [TestClass()]
+    public class BaseControllerTests
+    {
+        [TestMethod()]
+        public void GetUsernameTest()
+        {
+        }
+    }
+}
